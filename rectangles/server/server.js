e8dba6ba1c8bdbe07a040d3179c0b236e8ec694d
mikehillbilly <mikeadelaidehillbilly@gmail.com>
2024-05-16 17:25:45 +0930

    Latest commit

--- rectangles/server/server.js
+++ rectangles/server/server.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require ('express')
 const app = express()
 
 app.get("/api", (req, resp) => {
-    resp.json({"users": ["UserOne", "UserTwo"]});
+    resp.json({"users":["usrOne", "userTwo", "userThree", "userFour"]})
 })
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.listen(5000, () => {
+    console.log("Server started on port 5000")
+})
